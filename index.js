9dface0b23848e924e68c94db887a36228adcc21
Stephan L. Smith <stephanlsmith@Stephans-MacBook-Pro.local>
2016-01-22 09:58:00 -0500

    my first commit msg

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+var foo = require ('underscore')
+
+
+console.log ("hi 5")
+var s=1
+var t='foo'
+var o={ color:s, word:t}
+console.log("object",o)
+var a=[1,2,3,4,o]
+console.log(a)
+console.log(foo.last(a))
